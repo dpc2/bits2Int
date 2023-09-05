a2b1944fc648d5ce41f94a9cf763c38f1589b1fb
dpc2 <dpculler91@gmail.com>
2023-09-05 12:46:28 -0600

    Quality of life changes: spacing, q to quit, while loop to continually run the script

--- bit2Int.py
+++ bit2Int.py
@@ -1,51 +1,55 @@
 # This script calculates max values for signed and unsigned integers
+import sys
+
+if __name__ == "__main__":
+    while(1):
+        signedTotal = 0
+        unsignedTotal = 0
+        bits = input("How many bits? (q to exit)\n")
+        if bits == 'q':
+            sys.exit()
+        bits = int(bits)
+        x = 0
+
+        for i in range(0, bits-1):
+            x = 2 ** i
+            signedTotal = x + signedTotal
+
+        for i in range(0, bits):
+            x = 0
+            x =  2 ** i
+            unsignedTotal = x + unsignedTotal
 
 
-signedTotal = 0
-unsignedTotal = 0
-bits = input("How many bits?\n")
-bits = int(bits)
-x = 0
+        print("\nThe largest number we can represent with a signed " + str(bits) + " bit integer is:")
+        print(signedTotal)
+        print()
 
-for i in range(0, bits-1):
-    x = 2 ** i
-    signedTotal = x + signedTotal
-
-for i in range(0, bits):
-    x = 0
-    x =  2 ** i
-    unsignedTotal = x + unsignedTotal
-
-
-print("\nThe largest number we can represent with a signed " + str(bits) + " bit integer is:")
-print(signedTotal)
-print()
-
-print("The largest number we can represent with an unsigned " + str(bits) + " bit integer is:")
-print(unsignedTotal)
-print()
+        print("The largest number we can represent with an unsigned " + str(bits) + " bit integer is:")
+        print(unsignedTotal)
+        print()
 
 
 
-counter = bits - 1
-string1 = ''
+        counter = bits - 1
+        string1 = ''
 
-while counter >= 0:
-	string1 += '2^' + str(counter)
-	if counter != 0:
-		string1 += ' + '
-	counter -= 1
+        while counter >= 0:
+            string1 += '2^' + str(counter)
+            if counter != 0:
+                string1 += ' + '
+            counter -= 1
 
-print(string1)
+        print(string1)
 
 
-counter = bits - 1
-string2 = ''
+        counter = bits - 1
+        string2 = ''
 
-while counter >= 0:
-        string2 += str(2 ** counter)
-        if counter != 0:
-                string2 += ' + '
-        counter -= 1
+        while counter >= 0:
+                string2 += str(2 ** counter)
+                if counter != 0:
+                        string2 += ' + '
+                counter -= 1
 
-print(string2)
+        print(string2 + '\n')
